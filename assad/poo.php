5a2150aea145c2bacdff6a14841dd016df8d942c
lakroune <lakroune00@gmail.com>
2025-12-23 17:20:05 +0100

    feat: ajoute class Utilisateur

--- assad/poo.php
+++ assad/poo.php
@@ -21,4 +21,105 @@ class Connexion
     }
 }
 
+class Utilisateur
+{
+    protected int $id_utilisateur;
+    protected string $nom_utilisateur;
+    protected string $email;
+    protected string $mot_passe;
+    protected string $pays_utilisateur;
+    public function seconnecter(string $email, string $password)
+    {
+        $conn = (new Connexion())->connect();
+        if (!empty($email) and !empty($password)) {
+            $sql = "SELECT  * FROM utilisateurs  WHERE email = :email";
+
+            try {
+                $stmt = $conn->prepare($sql);
+            } catch (Exception $e) {
+                return "erreur sur sql";
+            }
+            $stmt->execute(["email" => $email]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($user) {
+                $hashedPassword = $user['motpasse_hash'];
+                if ($user["Approuver_utilisateur"]) {
+                    if (password_verify($password, $hashedPassword)) {
+                        session_start();
+                        $_SESSION['id_utilisateur'] = $user['id_utilisateur'];
+                        $_SESSION['nom_utilisateur'] = $user['nom_utilisateur'];
+                        $_SESSION['role_utilisateur'] = $user['role'];
+                        $_SESSION['logged_in'] = TRUE;
+                        return $user["role"];
+                    } else {
+                        return "mot pass invalid";
+                    }
+                } else {
+                    return "les utilisateur non approuve";
+                }
+            } else {
+                return "email n'exist pas ";
+            }
+            $stmt->close();
+        } else {
+            return "error les champs vide";
+        }
+    }
+}
+
+
+
+// $user = new Admin();
+// echo $user->seconnecter("admin@admin", "admin");
+// echo $user->approuver_guide(2);
+
+
+
+
+
+
+
+
+
+
+class Admin extends Utilisateur
+{
+    private string $role = "admin";
+
+    public function approuver_guide($id_utilisateur)
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "UPDATE utilisateurs SET Approuver_utilisateur = 1 WHERE role ='guide' and id_utilisateur = :id_utilisateur";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch (Exception $e) {
+            return "erreur sur sql";
+        }
+
+        if ($stmt->execute(["id_utilisateur" => $id_utilisateur])) {
+            return "utilisateur approuve avec succes";
+        } else {
+            return "erreur lors de l'approbation de l'utilisateur";
+        }
+        $stmt->close();
+    }
+}
+
+
+class Guide extends Utilisateur
+{
+    private string $role = "guide";
+    private int $Approuver_utilisateur;
+    private string $statut_utilisateur;
+}
+
+
+
+class Visiteur extends Utilisateur
+{
+    private string $role = "visiteur";
+    private int $Approuver_utilisateur;
+    private string $statut_utilisateur;
+}
+
 
