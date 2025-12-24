49aa3e0f269ad3481f5bf6f8e62a4b31b2824fc3
lakroune <lakroune00@gmail.com>
2025-12-24 11:08:54 +0100

    feat :ajout geter et seter pour class visiteur

--- assad/poo.php
+++ assad/poo.php
@@ -196,15 +196,13 @@ class Guide extends Utilisateur
     }
     public function setStatutUtilisateur(int $statut_utilisateur)
     {
-       if($statut_utilisateur == 0 || $statut_utilisateur == 1) {
+        if ($statut_utilisateur == 0 || $statut_utilisateur == 1) {
             $this->statut_utilisateur = $statut_utilisateur;
-       }
+        }
     }
-
-
 }
 
-    
+
 
 
 class Visiteur extends Utilisateur
@@ -212,6 +210,25 @@ class Visiteur extends Utilisateur
     private string $role = "visiteur";
     private int $Approuver_utilisateur;
     private string $statut_utilisateur;
+    public function __construct()
+    {
+        return parent::__construct();
+    }
+    public  function getRoleUtilisateur()
+    {
+        return $this->role;
+    }
+     
+    public function getStatutUtilisateur()
+    {
+        return $this->statut_utilisateur;
+    }
+    public function setStatutUtilisateur(int $statut_utilisateur)
+    {
+        if ($statut_utilisateur == 0 || $statut_utilisateur == 1) {
+            $this->statut_utilisateur = $statut_utilisateur;
+        }
+    }
 }
 
 
