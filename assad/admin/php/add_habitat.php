8705104fc6f559fb7e4a35221aec62b376d4a1ad
lakroune <lakroune00@gmail.com>
2025-12-26 16:29:21 +0100

    bug : coregier la virefication pour admin avans ajouter un habitat

--- assad/admin/php/add_habitat.php
+++ assad/admin/php/add_habitat.php
@@ -2,36 +2,30 @@
 
 
 
-require_once '../Class/Visiteur.php';
-require_once '../Class/Visite.php';
-require_once '../Class/Guide.php';
-require_once '../Class/Animal.php';
-require_once '../Class/Habitat.php';
-require_once '../Class/Reservation.php';
+require_once '../../Class/Admin.php';
+require_once '../../Class/Animal.php';
+require_once '../../Class/Habitat.php';
 
 
-if (
-    Visiteur::isConnected("admin")
-) {
+if (!Admin::isConnected("admin")) {
+    header("location: ../../connexion.php?message=access_denied");
+    exit();
+}
 
 
-    if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['nom_habitat']) && isset($_POST['type_climat']) && isset($_POST['description_habitat']) && isset($_POST['zone_zoo'])) {
-        $nom = htmlspecialchars($_POST['nom_habitat']);
-        $climat = htmlspecialchars($_POST['type_climat']);
-        $description = htmlspecialchars($_POST['description_habitat']);
-        $zone = htmlspecialchars($_POST['zone_zoo']);
+if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['nom_habitat']) && isset($_POST['type_climat']) && isset($_POST['description_habitat']) && isset($_POST['zone_zoo'])) {
+    $nom = htmlspecialchars($_POST['nom_habitat']);
+    $climat = htmlspecialchars($_POST['type_climat']);
+    $description = htmlspecialchars($_POST['description_habitat']);
+    $zone = htmlspecialchars($_POST['zone_zoo']);
 
-        $habitat = new Habitat();
+    $habitat = new Habitat();
 
-        if ($habitat->setNomHabitat($nom) && $habitat->setTypeClimat($climat) && $habitat->setDescriptionHabitat($description) && $habitat->setZoneZoo($zone) && $habitat->ajouterHabitat()) {
-            header("Location: ../admin_habitats.php?status=added");
-        } else {
-            header("Location: ../admin_habitats.php?error=sql_error");
-        }
+    if ($habitat->setNomHabitat($nom) && $habitat->setTypeClimat($climat) && $habitat->setDescriptionHabitat($description) && $habitat->setZoneZoo($zone) && $habitat->ajouterHabitat()) {
+        header("Location: ../admin_habitats.php?status=added");
     } else {
-        header("Location: ../admin_habitats.php?error=missing_data");
+        header("Location: ../admin_habitats.php?error=sql_error");
     }
 } else {
-    header("Location: ../admin_habitats.php?error=unauthorized");
+    header("Location: ../admin_habitats.php?error=missing_data");
 }
-exit();
