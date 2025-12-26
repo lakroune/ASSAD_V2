ae82b9151f53e0592c39ba5d9e9d835b6c2187bd
lakroune <lakroune00@gmail.com>
2025-12-26 16:40:09 +0100

    feat:  ajout la fonctionnaliter de mise  à jour  un habitat

--- assad/admin/php/update_habitat.php
+++ assad/admin/php/update_habitat.php
@@ -1,52 +1,42 @@
 <?php
-session_start();
-include "../../db_connect.php";
-if (
-    !isset($_SESSION['role_utilisateur'], $_SESSION['logged_in']) ||
-    $_SESSION['role_utilisateur'] !== "admin" ||
-    $_SESSION['logged_in'] !== true
-) {
-    header("Location: ../connexion.php?error=access_denied");
+
+
+
+require_once '../../Class/Admin.php';
+require_once '../../Class/Animal.php';
+require_once '../../Class/Habitat.php';
+
+
+if (!Admin::isConnected("admin")) {
+    header("location: ../../connexion.php?message=access_denied");
     exit();
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_habitat'])) {
 
-    $id_habitat = intval($_POST['id_habitat']);
-    $nom = htmlspecialchars(trim($_POST['nom_habitat']));
-    $climat = htmlspecialchars(trim($_POST['type_climat']));
-    $zone = htmlspecialchars(trim($_POST['zone_zoo']));
-    $description = htmlspecialchars(trim($_POST['description_habitat']));
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_habitat'], $_POST['nom_habitat'], $_POST['type_climat'], $_POST['zone_zoo'], $_POST['description_habitat']) and !empty($_POST['id_habitat']) && !empty($_POST['nom_habitat']) && !empty($_POST['type_climat']) && !empty($_POST['zone_zoo']) && !empty($_POST['description_habitat'])) {
 
-    if (!empty($nom) && !empty($climat) && !empty($zone)) {
+    // $id_habitat = ($_POST['id_habitat']);
+    // $nom = htmlspecialchars(trim($_POST['nom_habitat']));
+    // $climat = htmlspecialchars(trim($_POST['type_climat']));
+    // $zone = htmlspecialchars(trim($_POST['zone_zoo']));
+    // $description = htmlspecialchars(trim($_POST['description_habitat']));
+    $habitat = new Habitat();
 
-        $sql = "UPDATE habitats 
-                SET nom_habitat = ?, 
-                    type_climat = ?, 
-                    zone_zoo = ?, 
-                    description_habitat = ? 
-                WHERE id_habitat = ?";
-
-        $stmt = $conn->prepare($sql);
-
-        if ($stmt) {
-            $stmt->bind_param("ssssi", $nom, $climat, $zone, $description, $id_habitat);
-
-            if ($stmt->execute()) {
-                header("Location: ../admin_habitats.php?status=updated");
-            } else {
-                header("Location: ../admin_habitats.php?error=sql_error");
-            }
-            $stmt->close();
-        } else {
-            header("Location: ../admin_habitats.php?error=stmt_failed");
-        }
+    if (
+        $habitat->setIdHabitat($_POST["id_habitat"]) &&
+        $habitat->setNomHabitat(htmlspecialchars(trim($_POST['nom_habitat']))) &&
+        $habitat->setTypeClimat(htmlspecialchars(trim($_POST['type_climat']))) &&
+        $habitat->setZoneZoo(htmlspecialchars(trim($_POST['zone_zoo']))) &&
+        $habitat->setDescriptionHabitat(htmlspecialchars(trim($_POST['description_habitat'])))
+        && $habitat->modifierHabitat()
+    ) {
+        header("Location: ../admin_habitats.php?status=success");
+        exit();
     } else {
-        header("Location: ../admin_habitats.php?error=empty_fields");
+        header("Location: ../admin_habitats.php?status=error");
+        exit();
     }
 } else {
-    header("Location: ../admin_habitats.php");
+    header("Location: ../admin_habitats.php?status=error_input");
+    exit();
 }
-
-$conn->close();
-exit();
