808b303e1391081012cbd52c4949f8ce276c1d99
lakroune <lakroune00@gmail.com>
2025-12-26 15:40:37 +0100

    feat:  ajout la fonctionnaliter d'ajoute une habitat

--- assad/admin/php/add_habitat.php
+++ assad/admin/php/add_habitat.php
@@ -1,27 +1,36 @@
 <?php
-session_start();
-include "../../db_connect.php"; 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && $_SESSION['role_utilisateur'] === "admin") {
-    
-   
-    $nom = htmlspecialchars($_POST['nom_habitat']);
-    $climat = htmlspecialchars($_POST['type_climat']);
-    $description = htmlspecialchars($_POST['description_habitat']);
-    $zone = htmlspecialchars($_POST['zone_zoo']);
 
-    
-    $stmt = $conn->prepare("INSERT INTO habitats (nom_habitat, type_climat, description_habitat, zone_zoo) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $nom, $climat, $description, $zone);
 
-    if ($stmt->execute()) {
-        header("Location: ../admin_habitats.php?status=added");
+require_once '../Class/Visiteur.php';
+require_once '../Class/Visite.php';
+require_once '../Class/Guide.php';
+require_once '../Class/Animal.php';
+require_once '../Class/Habitat.php';
+require_once '../Class/Reservation.php';
+
+
+if (
+    Visiteur::isConnected("admin")
+) {
+
+
+    if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['nom_habitat']) && isset($_POST['type_climat']) && isset($_POST['description_habitat']) && isset($_POST['zone_zoo'])) {
+        $nom = htmlspecialchars($_POST['nom_habitat']);
+        $climat = htmlspecialchars($_POST['type_climat']);
+        $description = htmlspecialchars($_POST['description_habitat']);
+        $zone = htmlspecialchars($_POST['zone_zoo']);
+
+        $habitat = new Habitat();
+
+        if ($habitat->setNomHabitat($nom) && $habitat->setTypeClimat($climat) && $habitat->setDescriptionHabitat($description) && $habitat->setZoneZoo($zone) && $habitat->ajouterHabitat()) {
+            header("Location: ../admin_habitats.php?status=added");
+        } else {
+            header("Location: ../admin_habitats.php?error=sql_error");
+        }
     } else {
-        header("Location: ../admin_habitats.php?error=sql_error");
+        header("Location: ../admin_habitats.php?error=missing_data");
     }
-    
-    $stmt->close();
-    $conn->close();
 } else {
     header("Location: ../admin_habitats.php?error=unauthorized");
 }
